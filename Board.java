3f148f586457292000e0030f3a8cf78fb4cc33cc
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-02-16 22:11:27 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,13 +1,29 @@
 class Board{
-    protected int n;
     protected char[][] grid;
-    public Board(int n){
-
+    public Board(){
+        grid = new char[][]{{' ', ' ', ' '},
+                            {' ', ' ', ' '},
+                            {' ', ' ', ' '}};
     }
-    public boolean update_board(int x, int y, char symbol){
+    public void display_board(){
+        System.out.println("_______");
+        for (int i = 0; i < 3; i++) {
+            System.out.print('|');
+            for (int j = 0; j < 3; j++) {
+                System.out.print((grid[i][j]));
+                System.out.print('|');
+            }
+            System.out.println("\n_______");
+
+        }
+    }
+    public void update_board(int x, int y, char symbol){
+        grid[x][y] = symbol;
+    }
+    public boolean is_winner(){
         return false;
     }
-    public boolean is_winner(char symbol){
+    public boolean is_draw(){
         return false;
     }
 }

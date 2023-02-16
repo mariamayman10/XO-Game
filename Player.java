046902bca215bcf2258a1d13730d3b768e943bc6
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-02-16 22:12:33 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,16 +1,34 @@
-class Player{
-    protected String name;
-    protected char symbol;
-    public Player(String name, char symbol){
+import java.util.Scanner;
 
+class Player extends Board{
+    protected String Name;
+    protected char Symbol;
+    public Player(String name, char symbol){
+        Name = name;
+        Symbol = symbol;
     }
     public void get_move(int x, int y){
-
+        Scanner in = new Scanner(System.in);
+        while(x > 2 || y > 2){
+            System.out.println("Invalid Coordinate");
+            System.out.print("Enter x: ");
+            x = in.nextInt();
+            System.out.print("\nEnter y: ");
+            y = in.nextInt();
+        }
+        if(grid[x][y] != ' '){
+            System.out.println("Invalid Coordinate");
+            System.out.print("Enter x: ");
+            x = in.nextInt();
+            System.out.print("\nEnter y: ");
+            y = in.nextInt();
+        }
+        update_board(x, y, this.Symbol);
     }
     public char getSymbol(){
-        return symbol;
+        return Symbol;
     }
     public String getName(){
-        return name;
+        return Name;
     }
 }

771b3dae8c872a9f79232f1421238f9d722a0815
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-02-16 22:11:04 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-
+        Game XO = new Game();
+        XO.play_game();
     }
 }
+

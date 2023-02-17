1d9a1d9035adf3b1cfe979a55a3dcc2fc0d58dd1
Shrouk Heshmat Abu El-Safa Abd El-hady <102330882+shroukheshmat1@users.noreply.github.com>
2023-02-17 12:32:37 +0200

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,8 +2,8 @@ class Board{
     protected char[][] grid;
     public Board(){
         grid = new char[][]{{' ', ' ', ' '},
-                            {' ', ' ', ' '},
-                            {' ', ' ', ' '}};
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}};
     }
     public void display_board(){
         System.out.println("_______");
@@ -20,10 +20,54 @@ class Board{
     public void update_board(int x, int y, char symbol){
         grid[x][y] = symbol;
     }
-    public boolean is_winner(){
+    public boolean is_winner()
+    {
+        //row win
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+
+                if((grid[i][j] == grid[i][j+1])&&(grid[i][j]!='-'))
+                {
+                    if (j!=1)
+                        continue;
+                    else if (j==1)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+        }
+        //col win
+        int j=0;
+        for (int i = 0; i < 2; i++)
+        {
+            if ((grid[i][j]==grid[i+1][j])&&(grid[i][j]!='-'))
+            {
+                if (i!=1)
+                {
+                    j++;
+                    continue;
+                }
+                else if (i==1)
+                    return true;
+            }
+        }
+        //diagonal win
+        if (((grid[0][0]==grid[1][1])&&(grid[0][0]==grid[2][2]))||(grid[0][2]==grid[1][1])&&(grid[0][2]==grid[2][0]))
+        {
+            if (grid[1][1]!='-')
+                return true;
+        }
         return false;
     }
-    public boolean is_draw(){
-        return false;
+    public boolean is_draw()
+    {
+        boolean result=is_winner();
+        if (result==false)
+            return false;
+        return true;
     }
 }
